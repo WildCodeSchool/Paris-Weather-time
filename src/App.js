8be15350fa4bec71517f54485beac40a509507b2
Agueda Perez <aguedapm@yahoo.com>
2019-04-05 15:09:55 +0200

    Created Weather Div and linked it to API

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './components/WeatherDiv'
+import WeatherDiv from './components/WeatherDiv';
 
 class App extends Component {
+  state = {
+    weatherData : [],
+    lo: true,
+  }
+
+  getWeather = () => {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=Paris&lang=fr&units=metric&appid=e8fc88dd5f1edd7f7ff6a9a5be06bd83')
+    .then(res => res.json()) 
+    .then(res => this.setState({weatherData: res, lo: false}))
+    // .then(res => this.state.lo ? console.log("pipi")
+    // : console.log('totototo',this.state.weatherData.weather[0]))
+  }
+
+  componentDidMount() {
+    this.getWeather()
+    }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+    if ( this.state.lo) {
+        return (<div>loading</div>)
+    } else {
+        return (
+            <div>
+                <WeatherDiv weatherData={this.state.weatherData}/>
+            </div>
+        )
+    }
+
   }
 }
 

2fef7b7307e79cbaab43b75929d550662b52fa57
Nocktiss <anthony.grupposo@gmail.com>
2019-04-05 11:09:52 +0200

    Add 3 components icons + file scss + 3 pictures/icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BtnFilter from "./components/BtnFilter"
+import BtnLogin from "./components/BtnLogin"
+import BtnFavorite from "./components/BtnFavorite"
+import './components/BtnNavBar.scss';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <BtnFilter />
+          <BtnLogin />
+          <BtnFavorite />
+
         </header>
       </div>
     );

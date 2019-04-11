39e5a178f81b391501cf47e9806bc5538da8506a
Agueda Perez <aguedapm@yahoo.com>
2019-04-11 10:58:05 +0200

    Added geolocalisation to WeatherDiv

--- src/App.js
+++ src/App.js
@@ -13,24 +13,45 @@ import './components/BtnNavBar.scss';
 class App extends Component {
     state = {
         weatherData: [],
-        lo: true,
+        loading: true,
+        lat: '',
+        lon: ''
     }
 
-    getWeather = () => {
-        fetch('http://api.openweathermap.org/data/2.5/weather?q=Paris&lang=fr&units=metric&appid=e8fc88dd5f1edd7f7ff6a9a5be06bd83')
-            .then(res => res.json())
-            .then(res => this.setState({ weatherData: res, lo: false }))
-        // .then(res => this.state.lo ? console.log("pipi")
-        // : console.log('totototo',this.state.weatherData.weather[0]))
+    //fonction qui permet d'obtenir la localisation
+    getLocation = () => {
+        //si l'utilisateur accepte d'être géolocalisé
+        if (navigator.geolocation) {
+        //getCurrentPosition permet de retourner un objet position qui donne notamment les coordonnées
+            navigator.geolocation.getCurrentPosition(position => {
+                this.setState({ lon: position.coords.longitude, lat: position.coords.latitude})
+        //Attention cette ligne est importante, elle permet de s'assurer que getWeather ne s'active que après avoir obtenu la geolocalisation
+                this.getWeather()            
+            }) 
+        } else {
+        //alerte qui s'active si l'utilisateur n'accepte pas d'être géolocalisé
+            alert("Trop nul")
+        }
     }
+    // fonction qui recherche l'API météo
+    getWeather = () => {
+
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&lang=fr&units=metric&appid=e8fc88dd5f1edd7f7ff6a9a5be06bd83`)
+            .then(res => res.json())
+    //permet de remplir le tableau weatherData dans le state et de desactiver loading - voir le if/else au début du render
+            .then(res => this.setState({ weatherData: res, loading: false }))
+    }
+
+    //
 
     componentDidMount() {
-        this.getWeather()
+        this.getLocation()
     }
 
     render() {
 
-        if (this.state.lo) {
+    //le if/else, permet de s'assurer que le render ne s'active que quand l'API a bien chargé ses données dans le state et donc transformé loading en false
+        if (this.state.loading) {
             return (<div>loading</div>)
         } else {
             return (
@@ -39,7 +60,6 @@ class App extends Component {
                         <BtnFilter />
                         <BtnLogin />
                         <BtnFavorite />
-
                     </header>
                     <WeatherDiv weatherData={this.state.weatherData} />
 

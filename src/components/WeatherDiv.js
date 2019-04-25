95dbbcbc8c49161c6f3f17a4636705f21785126c
aguedapm <47628947+aguedapm@users.noreply.github.com>
2019-04-25 17:05:56 +0200

    Revert "Revert 11 includejsonfileinsimpleslider"

--- src/components/WeatherDiv.js
+++ src/components/WeatherDiv.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './WeatherDiv.css';
 
 
-
+//l'objet Weather Data est celui qui est passé par le props de la balise WeatherDiv dans App.js. C'est un objet retourné dans le state par l'API
 const WeatherDiv = ({weatherData}) =>{
     const icon = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png"
    
     return (
-        <div>
-            <h1 className="City">{weatherData.name}</h1>
-            <img className="WeatherImage" src={icon} alt="Weather"/>
-            <p className="Description">{weatherData.weather[0].description}</p>
-            <p className="Description">{weatherData.main.temp} °C</p>
+        <div className="weatherDiv">
+            <h1 className="city">{weatherData.name}</h1>
+            <img className="weatherImage" src={icon} alt="Weather"/>
+            <p className="description">{weatherData.weather[0].description}</p>
+            <p className="description">{weatherData.main.temp} °C</p>
         </div>
     )
 

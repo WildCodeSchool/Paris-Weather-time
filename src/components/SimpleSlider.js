452b919e744125438dd1ef4a913856f1b64d9f1c
Rémy Jacob <rjpro77@outlook.fr>
2019-04-26 14:19:08 +0200

    insert mode repeat in the setting slider

--- src/components/SimpleSlider.js
+++ src/components/SimpleSlider.js
@@ -1,53 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
 import Slider from "react-slick";
-import './SimpleSlider.css';
 import ActivityCards from "./ActivityCards";
 import Activity from "./Activity.json"
+import './SimpleSlider.css';
 
-
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 1500,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  initialSlide: 0,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        initialSlide: 2,
-        infinite: true
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true
-      }
+class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 1500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+            infinite: true
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            repeat: 99
+          }
+        }
+      ]
     }
-  ]
-};
 
-const SimpleSlider = () =>
-  <Slider {...settings}>
-    {Activity.map((data) => <ActivityCards {...data} />)}
-  </Slider>
+    return (
+      
+      
+      <Slider {...settings}>
+        {Activity.map((data) => <ActivityCards {...data} />)}
+      </Slider >
+    
+    )
+  }
+}
 
 export default SimpleSlider
+
 
 
 

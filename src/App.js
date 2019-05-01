8aa81e368c4254ce03c5f320eec9466b6c47d254
Nocktiss <anthony.grupposo@gmail.com>
2019-05-01 14:18:33 +0200

    simple button for 1 modal filter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
-import { Route, Switch } from "react-router-dom"
 
 import Footer from './components/Footer';
 import Logo from "./components/Logo"
-import EffectModalFilter from "./components/EffectModalFilter"
 import SimpleSlider from "./components/SimpleSlider"
 import WeatherAPI from './components/WeatherAPI';
 // import Contact from './components/Contact'
-// import NavBarButton from './components/NavBarButton';
+import NavBarButton from './components/NavBarButton';
 
-import BtnFilter from "./screen/BtnFilter"
-import BtnProfil from "./screen/BtnProfil"
-// import BtnFavorites from "./screen/BtnFavorites"
 
 import './components/ModalofContact.css';
 import './App.css';
 import './components/BtnNavBar.scss';
 import './components/WeatherDiv';
-// import NavBarButton from './components/NavBarButton';
 
 class App extends Component {
   state = {
@@ -27,12 +21,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <div className="l"><Logo /></div>
-          <Switch>
-            <Route exact path="/" component={App} />
-            {/* <Route path="/filtre" component={Logo} /> */}
-            <Route path="/profil" component={BtnFilter} />
-            <Route path="/favoris" component={BtnProfil} />
-          </Switch>
+        <NavBarButton/>
         </header>
         <div className="WeatherAPI"><WeatherAPI /></div>
         <SimpleSlider />
--- src/components/NavBarButton.js
+++ src/components/NavBarButton.js
@@ -1,74 +1,43 @@
 import React, { Component } from "react";
-import favorites from "../images/favorites.png"
-import profil from "../images/profil.png"
-import filtre from "../images/filtre.png"
+
 import EffectModalFilter from './EffectModalFilter';
+import filtre from '../images/filtre.png'
+
+import '../components/BtnNavBar.scss'
 
 
-class NavBarButton extends Component {
-
+class NavbarButton extends Component {
 
     state = {
-        isShowing: false,
-        btn1: false,
-        btn2: false,
-        btn3: false,
-
+        isShowing: false
     }
 
-    /*-- Open Modal --*/
-    openModalHandler = (param) => {
-        if ("btn1" === param) {
-            this.setState({ isShowing: true, btn1: true, btn2: false, btn3: false })
-        } else if ("btn2" === param) {
-            this.setState({ isShowing: true, btn1: false, btn2: true, btn3: false })
-        } else if ("btn3" === param) {
-            this.setState({ isShowing: true, btn1: false, btn2: false, btn3: true })
-        }
+    // fonction de l'ouverture de la modal
+    openModalHandler = () => {
+        this.setState({
+            isShowing: true
+        });
     }
-
-    /*-- Close Modal --*/
-    closeModalHandler = (param) => {
-        if ("btn1" === param) {
-            this.setState({ isShowing: false, btn1: false, btn2: false, btn3: false })
-        } else if ("btn2" === param) {
-            this.setState({ isShowing: false, btn1: false, btn2: false, btn3: false })
-        } else if ("btn3" === param) {
-            this.setState({ isShowing: false, btn1: false, btn2: false, btn3: false })
-        }
+    // fonction de la fermeture de la modal
+    closeModalHandler = () => {
+        this.setState({
+            isShowing: false
+        });
     }
-
     render() {
-        const { btn1, btn2, none } = this.state
         return (
-            <div className="">
-                <button className="btn-filter" onClick={this.state.isShowing ? () => this.closeModalHandler("btn1") : () => this.openModalHandler("btn1")}><img src={filtre} className="icon" alt="logo" /></button>
-                <button className="btn-profil" onClick={this.state.isShowing ? () => this.closeModalHandler("btn2") : () => this.openModalHandler("btn2")}><img src={profil} className="icon" alt="logo" /></button>
-                <button className="btn-favorites" onClick={this.state.isShowing ? () => this.closeModalHandler("btn3") : () => this.openModalHandler("btn3")}><img src={favorites} className="icon" alt="logo" /></button>
-                    {
-                        btn1 === true ?
-                    <EffectModalFilter
-                        show={this.state.isShowing}
-                        close={this.closeModalHandler}>
-                        Filtre
-                        <button className="applyfilter">Appliquer Filtres</button>
-                    </EffectModalFilter> :
-                    <EffectModalFilter
-                        show={this.state.isShowing}
-                        close={this.closeModalHandler}>
-                        Profil
-                    </EffectModalFilter>
-                } {
-                    btn2 === none ?
-                    <EffectModalFilter
+            <div>
+                <header className="btn-filter">
+                    <img src={filtre} onClick={this.state.isShowing ? this.closeModalHandler : this.openModalHandler} className="icon" alt="logo" />
+                </header>
+                <EffectModalFilter
                     show={this.state.isShowing}
                     close={this.closeModalHandler}>
-                    Profil
-                </EffectModalFilter> : none
-                }
+                    ksjscj
+                </EffectModalFilter>
             </div>
         )
     }
 }
 
-export default NavBarButton;
+export default NavbarButton;
